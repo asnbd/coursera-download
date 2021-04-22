c6edc3e196f18ccb6d4cd38702ed4d86a1a097d0
Asswad Sarker Nomaan <32814117+asnbd@users.noreply.github.com>
2021-04-22 20:50:15 +0600

    Add Bot.py

---
+++ Bot.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+import os
+import utils
+
+class Bot:
+    def __init__(self, name):
+        self.profile_name = name
+        profile_dir = os.path.join(os.getcwd(), "profiles", self.profile_name)
+
+        chrome_options = webdriver.ChromeOptions()
+
+        chrome_options.set_capability("pageLoadStrategy", "none")
+        chrome_options.set_capability("webdriver.load.strategy", "none")
+        chrome_options.add_argument("user-data-dir=" + profile_dir)
+
+        self.driver = webdriver.Chrome(executable_path="chromedriver_win32/chromedriver.exe", options=chrome_options)  # , desired_capabilities=capabilities
+
+    ###################################################################################################################
+    """" Action Functions """
+    ###################################################################################################################
+    def loadUrl(self, url):
+        self.driver.get(url)
+
+    def back(self):
+        # TODO implement browser back
+        pass
+
+    ###################################################################################################################
+    """" Driver Functions """
+    ###################################################################################################################
+    def loadHtml(self, html_content):
+        self.driver.get("data:text/html;charset=utf-8," + html_content)
+
+    def executeScript(self, script):
+        try:
+            return self.driver.execute_script(script)
+        except Exception as e:
+            utils.log(e)
+            return None
+
+    def closeBrowser(self):
+        self.driver.close()
+        self.driver.quit()
+
