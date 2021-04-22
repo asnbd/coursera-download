8a8082e45b61645427f965df78cbadcd2b7d7fa8
Asswad Sarker Nomaan <32814117+asnbd@users.noreply.github.com>
2021-04-22 20:46:40 +0600

    Added utilities

---
+++ utils.py
@@ -0,0 +1,54 @@
+from time import localtime
+from datetime import datetime
+from threading import Thread
+import winsound
+
+
+def formatTime(time_in_sec):
+    r_time_in_sec = round(time_in_sec)
+    hours = r_time_in_sec // 3600
+    minutes = r_time_in_sec // 60 % 60
+    seconds = r_time_in_sec % 60
+    return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+
+
+def getFormattedTime(time_in_sec):
+    time_struct = localtime(time_in_sec)
+    hours = time_struct.tm_hour
+    minutes = time_struct.tm_min
+    seconds = time_struct.tm_sec
+    return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+
+
+def getCurrentTime():
+    return datetime.now()
+
+
+def getFormattedDateTimeFile(time_in_sec):
+    time_struct = localtime(time_in_sec)
+
+    year = time_struct.tm_year
+    month = time_struct.tm_mon
+    day = time_struct.tm_mday
+
+    hours = time_struct.tm_hour
+    minutes = time_struct.tm_min
+    seconds = time_struct.tm_sec
+
+    return "{:02d}{:02d}{:02d}_{:02d}{:02d}{:02d}".format(year, month, day, hours, minutes, seconds)
+
+
+def playBeep():
+    Thread(target=runPlayBeepThread).start()
+
+
+def playShortBeep():
+    Thread(target=runPlayBeepThread, args=(1500, 90)).start()
+
+
+def runPlayBeepThread(frequency=2000, duration=200):
+    winsound.Beep(frequency, duration)
+
+
+def log(text):
+    print("[{}] {}".format(getCurrentTime(), text))
