0b7f421af6c12a200be79ad0702cf78b01e40b71
Asswad Sarker Nomaan <32814117+asnbd@users.noreply.github.com>
2021-04-24 01:10:54 +0600

    Update createInputFrame

--- gui.py
+++ gui.py
@@ -52,12 +52,31 @@ class App(tk.Tk):
         course_link_entry = tk.Entry(labelframe, width=100)
 
         self.refresh_button = tk.Button(labelframe, text="Load", width=8)
-        self.refresh_button.config(command=self.refreshAllDeviceList)
+        # self.refresh_button.config(command=self.load)
+
+        self.get_video_check_var = tk.IntVar(value=1)
+        self.get_reading_check_var = tk.IntVar(value=1)
+        self.get_quiz_check_var = tk.IntVar(value=1)
+        self.get_graded_check_var = tk.IntVar(value=1)
+
+        checkboxes_frame = tk.Frame(labelframe)
+        get_video_check_box = tk.Checkbutton(checkboxes_frame, text="Video", variable=self.get_video_check_var)
+        get_reading_check_box = tk.Checkbutton(checkboxes_frame, text="Reading", variable=self.get_reading_check_var)
+        get_quiz_check_box = tk.Checkbutton(checkboxes_frame, text="Quiz", variable=self.get_quiz_check_var)
+        get_graded_check_box = tk.Checkbutton(checkboxes_frame, text="Peer Graded Assignment", variable=self.get_graded_check_var)
 
         padding_y = 3
 
-        course_link_label.grid(row=0, column=0, padx=5, pady=padding_y + 2, sticky=tk.E + tk.N)
-        course_link_entry.grid(row=0, column=1, padx=5, pady=padding_y + 2, sticky=tk.E + tk.N)
+        course_link_label.grid(row=0, column=0, padx=5, pady=padding_y + 2, sticky=tk.S + tk.N)
+        course_link_entry.grid(row=0, column=1, padx=5, pady=padding_y + 2, sticky=tk.S + tk.N)
+        self.refresh_button.grid(row=0, column=2, padx=5, pady=padding_y + 2, sticky=tk.S + tk.N)
+
+        # Checkboxes Frame
+        checkboxes_frame.grid(row=1, column=1, pady=padding_y + 2, sticky=tk.S + tk.N + tk.W)
+        get_video_check_box.grid(row=0, column=0, padx=5, sticky=tk.S + tk.N + tk.W)
+        get_reading_check_box.grid(row=0, column=1, padx=5, sticky=tk.S + tk.N + tk.W)
+        get_quiz_check_box.grid(row=0, column=2, padx=5, sticky=tk.S + tk.N + tk.W)
+        get_graded_check_box.grid(row=0, column=3, padx=5, sticky=tk.S + tk.N + tk.W)
 
 
 if __name__ == "__main__":
